9c348acc35e9a6f8a9cb4eb3292f5626a29059a2
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-20 17:49:46 +0900

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -8,7 +8,25 @@ hello <- function(myname = ""){
   if(myname == ""){
     stop("Tell me your name!")
   }
+  
+  library(rvest)
+
+countryListURL <- vector();
+countryListNames <- vector();
+
+URL <- paste("http://int.soccerway.com/competitions/?ICID=TN_02", sep="")
+WS <- read_html(URL)
+
+rawTableDataNewNew <- WS %>%
+  html_nodes(xpath = "//div[@id='page_competitions_1_block_competitions_index_club_domestic_4']//div[@class='row']//a")
+
+countryListNames <- html_text(rawTableDataNewNew)
+countryListURL <- html_attr(rawTableDataNewNew, "href")
+  
+  randomCountry <- gsub("\n","",gsub("  ","",countryListNames[sample(1:(length(countryListNames)), 1)]))
+
+  
   list(
-    message = paste("hello", myname, "! This certainly is", R.Version()$version.string)
+    message = paste("hello ",myname," in ", randomCountry, "! This certainly is", R.Version()$version.string)
   )
 }
