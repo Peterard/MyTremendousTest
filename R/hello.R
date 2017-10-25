f4fccfbd4f701d54806e62aa34b46ac8f575f77c
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-25 16:30:40 +0900

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -14,10 +14,31 @@ hello <- function(myname = ""){
 URL <- paste("http://int.soccerway.com/",myname,"/matches/", sep="")
 WS <- read_html(URL)
 
-rawTableData <- WS %>%
+rawTableDataDate <- WS %>%
+  html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]//tbody//tr[contains(@class, 'match')]//td[contains(@class, 'full-date')]")
+
+rawTableDataHomeTeam <- WS %>%
+  html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]//tbody//tr[contains(@class, 'match')]//td[contains(@class, 'team-a')]//a")
+
+rawTableDataScore <- WS %>%
+  html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]//tbody//tr[contains(@class, 'match')]//td[contains(@class, 'score')]//a")
+
+rawTableDataAwayTeam <- WS %>%
+  html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]//tbody//tr[contains(@class, 'match')]//td[contains(@class, 'team-b')]//a")
+
+rawTableDataTable <- WS %>%
   html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]")
 
-scores <- html_table(rawTableData)
+rawTableDataCompetition <- WS %>%
+  html_nodes(xpath = "//div[@id='page_team_1_block_team_matches_3']//table[contains(@class, 'matches')]//tbody//tr[contains(@class, 'match')]//td[contains(@class, 'competition')]//a")
+
+scores <- data.frame(Date = html_text(rawTableDataDate), 
+           Competition =  html_attr(rawTableDataCompetition, "title"),
+           HomeTeam = html_attr(rawTableDataHomeTeam, "title"),
+           Score = gsub("\n","",gsub(" ","",html_text(rawTableDataScore))),
+           AwayTeam = html_attr(rawTableDataAwayTeam, "title"),
+           Result = html_attr(rawTableDataScore, "class"),
+           Url = html_attr(rawTableDataScore, "href"))
   
   list(
     message = scores
