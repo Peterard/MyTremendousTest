254c46fe20a1e14742a81364c181bc308b4c653c
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-26 11:25:01 +0900

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -36,7 +36,7 @@ rawTableDataCompetition <- WS %>%
   matchDates <- as.Date(html_text(rawTableDataDate), "%d/%m/%y")
   today <- as.Date(Sys.Date(), "%d/%m/%y")
   cutoff <- seq(today, length = 2, by = "-4 months")[2]
-  matchSelector <- matchDates >= cutoff
+  matchSelector <- matchDates >= cutoff & matchDates <= today
   
 scores <- data.frame(Date = html_text(rawTableDataDate[matchSelector]), 
            Competition =  html_attr(rawTableDataCompetition[matchSelector], "title"),
