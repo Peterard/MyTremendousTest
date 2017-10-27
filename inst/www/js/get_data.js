58755d289524a5424613374a9605bec8ec7b6694
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-27 11:30:33 +0900

    Add files via upload

--- inst/www/js/get_data.js
+++ inst/www/js/get_data.js
@@ -11,20 +11,12 @@ function getGoalsScored(){
     var homeTeam = $(this).parents(".match-result").find(".home-team").text().replace("  ","");
     var awayTeam = $(this).parents(".match-result").find(".away-team").text().replace("  ","");
     homeTeam = homeTeam.slice(1, homeTeam.length - 1);
-    awayTeam = awayTeam.slice(1, awayTeam.length - 1);
-    console.log("Teams:")
-    console.log("'"+homeTeam+"'")
-    console.log("'"+awayTeam+"'")
-    console.log("'"+$("#team-select option:selected").text()+"'")
-    console.log(awayTeam　== $("#team-select option:selected").text())
-    console.log(awayTeam　=== $("#team-select option:selected").text())
+    awayTeam = awayTeam.slice(1, awayTeam.length);
     if($("#team-select option:selected").text() === homeTeam){
       goalscored = Number(score.replace("P", "").replace("E","").split("-")[0]);
-      console.log(goalscored)
       opponent = awayTeam;
     }else if($("#team-select option:selected").text() === awayTeam){
       goalscored = Number(score.replace("P", "").replace("E","").split("-")[1]);
-      console.log(goalscored)
       opponent = homeTeam;
     }
     var arrayCounter = goalArray.length;
