7b71a295dbad71cf5cdec455df614132eb583526
Peterard <32120920+Peterard@users.noreply.github.com>
2017-10-27 13:32:42 +0900

    Add files via upload

--- inst/www/js/chart_creator.js
+++ inst/www/js/chart_creator.js
@@ -86,15 +86,3 @@ var myLineChart = new Chart(ctx, {
   options: options
 });
 }
-
-$("#goals-scored").on("click", function(){
-  chartCreator(getGoalsScored());
-})
-
-$("#goals-conceded").on("click", function(){
-  chartCreator(getGoalsConceded());
-})
-
-$("#goals-difference").on("click", function(){
-  chartCreator(getGoalDifference());
-})
